9836c35818a8e4d669bcd45a1bb06c80f25184cf
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-17 11:54:43 -0300

    style: use prettier to format code

--- src/components/SignForm/index.jsx
+++ src/components/SignForm/index.jsx
@@ -1,30 +1,42 @@
-import { Button } from 'antd'
-import './index.css'
-import { useForm } from 'react-hook-form'
-import Api from '../../services/Api'
+import "./index.css";
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import Api from "../../services/Api";
+import { useNavigate } from "react-router-dom";
 
 export default function SignForm() {
-  const { register, handleSubmit } = useForm()
-
-  const submit = data => {
-    console.log(data)
-    Api.post('/auth', data)
-      .then(({data}) => {
-        console.log(data)
-        sessionStorage.setItem('token', data.token)
-        sessionStorage.setItem('userId', data.userId)
-        
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const submit = (data) => {
+    console.log(data);
+    Api.post("/auth", data)
+      .then(({ data }) => {
+        sessionStorage.setItem("token", data.token);
+        sessionStorage.setItem("userId", data.userId);
+        navigate("/todos");
       })
-      .catch(error => console.log(error))
-  }
+      .catch((error) => alert("Username ou senha incorretas."));
+  };
   return (
-    <div className='form-container'>
+    <div className="form-container">
       <h2>Entrar</h2>
-      <form onSubmit={handleSubmit(submit)} className='sign-in-form'>
-        <input name='username' type='text' {...register('username')} placeholder='Username' />
-        <input name='password' type='password' {...register('password')} placeholder='Password' />
-        <Button type='primary' htmlType='submit'>Entrar</Button>
+      <form onSubmit={handleSubmit(submit)} className="sign-in-form">
+        <input
+          name="username"
+          type="text"
+          {...register("username")}
+          placeholder="Username"
+        />
+        <input
+          name="password"
+          type="password"
+          {...register("password")}
+          placeholder="Password"
+        />
+        <Button type="primary" htmlType="submit">
+          Entrar
+        </Button>
       </form>
     </div>
-  )
+  );
 }
