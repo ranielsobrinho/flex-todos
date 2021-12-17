a880732459abe32a05a5713db2a89c0cf53a52b3
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-17 09:16:41 -0300

    feat: use axios to post data from form to api

--- src/components/SignForm/index.jsx
+++ src/components/SignForm/index.jsx
@@ -1,15 +1,24 @@
 import { Button } from 'antd'
 import './index.css'
 import { useForm } from 'react-hook-form'
+import Api from '../../services/Api'
 
 export default function SignForm() {
   const { register, handleSubmit } = useForm()
 
   const submit = data => {
     console.log(data)
+    Api.post('/auth', data)
+      .then(({data}) => {
+        console.log(data)
+        sessionStorage.setItem('token', data.token)
+        sessionStorage.setItem('userId', data.userId)
+        
+      })
+      .catch(error => console.log(error))
   }
   return (
-    <div className='sign-in-container'>
+    <div className='form-container'>
       <h2>Entrar</h2>
       <form onSubmit={handleSubmit(submit)} className='sign-in-form'>
         <input name='username' type='text' {...register('username')} placeholder='Username' />
