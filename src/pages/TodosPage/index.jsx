d66fde856d003c7b27049e216ba3c6a04f132b16
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-17 11:54:23 -0300

    feat: add header to page component

---
+++ src/pages/TodosPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+export default function TodosPage() {
+  return (
+    <div>
+      <Header />
+      <h2>Todos here</h2>
+    </div>
+  )
+}
