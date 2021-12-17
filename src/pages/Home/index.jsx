03d417308dd14bfd17eae6b7bb364b4eef0ee60a
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-17 09:15:32 -0300

    refactor: add login form and register form

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,39 @@
-import Api from "../../services/Api"
 import './index.css'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import SignForm from "../../components/SignForm"
+import RegisterForm from "../../components/RegisterForm"
+import { Button } from "antd"
 
 export default function Home() {
-  const [users, setUsers] = useState([])
-  useEffect(() => {
-    Api.get('/users')
-      .then(({ data }) => {
-        setUsers(data.data)
-        console.log(users)
-      })
-      .catch(error => console.log(error))
-  })
+  const [ login, setLogin ] = useState(true)
+
+  function toggleLogin(){
+    setLogin(!login)
+  }
 
   return (
     <div className="container">
-      <SignForm />
+      {login ? 
+        <div className='form'>
+          <SignForm />
+          <div className='content'>
+            <p>
+              Não tem conta?
+            </p>
+            <Button type='link' onClick={() => {toggleLogin()}}>Registre-se aqui.</Button>
+          </div>
+        </div>
+      :
+        <div className='form'>
+          <RegisterForm />
+          <div className='content'>
+            <p>
+              Já tem conta?
+            </p>
+            <Button type='link' onClick={() => {toggleLogin()}}>Faça seu login.</Button>
+          </div>
+        </div>
+      }
     </div>
   )
 }
