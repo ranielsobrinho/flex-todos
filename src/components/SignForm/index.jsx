24d772319613e84ecbeec3e3447b402db7b06903
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-16 23:49:30 -0300

    feat: add form component with react hook form and styling with css

--- src/components/SignForm/index.jsx
+++ src/components/SignForm/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Button } from 'antd'
+import './index.css'
+import { useForm } from 'react-hook-form'
 
 export default function SignForm() {
-  return (
-    <div>
+  const { register, handleSubmit } = useForm()
 
+  const submit = data => {
+    console.log(data)
+  }
+  return (
+    <div className='sign-in-container'>
+      <h2>Entrar</h2>
+      <form onSubmit={handleSubmit(submit)} className='sign-in-form'>
+        <input name='username' type='text' {...register('username')} placeholder='Username' />
+        <input name='password' type='password' {...register('password')} placeholder='Password' />
+        <Button type='primary' htmlType='submit'>Entrar</Button>
+      </form>
     </div>
   )
 }
