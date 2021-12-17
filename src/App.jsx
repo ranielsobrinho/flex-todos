358addf9f52dd259758d53a68b4706a0f7e2d161
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-17 11:51:23 -0300

    feat: add new route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home';
+import TodosPage from './pages/TodosPage'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' exact element={<Home />} />
+        <Route path='/todos' exact element={<TodosPage />} />
       </Routes>
     </Router>
   );
