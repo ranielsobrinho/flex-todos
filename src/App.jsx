49302f3af98b846b6f3346a270bba6d9baaff192
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-20 10:22:44 -0300

    feat: add a new route to edit todo and use prettier to format the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home';
-import TodosPage from './pages/TodosPage'
+import Home from "./pages/Home";
+import TodosPage from "./pages/TodosPage";
+import EditTodo from "./pages/EditTodo";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' exact element={<Home />} />
-        <Route path='/todos' exact element={<TodosPage />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/todos" exact element={<TodosPage />} />
+        <Route path="/todos/:id" exact element={<EditTodo />} />
       </Routes>
     </Router>
   );
