35c90c8fe52363eff50595fce300f7aa2ffe24a6
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-12-20 10:25:38 -0300

    feat: add button that goes to edit page

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
-import { Card } from "antd";
+import { Button, Card } from "antd";
 import Api from "../../services/Api";
+import { useNavigate } from "react-router-dom";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
   const id = sessionStorage.getItem("userId");
+  const navigate = useNavigate();
 
   useEffect(() => {
     Api.get(`/users/${id}`)
@@ -14,12 +16,26 @@ export default function Todo() {
       })
       .catch((err) => console.log(err));
   });
+
+  function editTodo(id) {
+    navigate(`/todos/${id}`);
+  }
   return (
     <div className="todo-container">
       {todos.map((todo) => {
         return (
-          <Card key={todo.id}>
+          <Card key={todo.id} className="card-content">
             <p>{todo.content}</p>
+            <div>
+              <Button
+                type="link"
+                onClick={() => {
+                  editTodo(todo.id);
+                }}
+              >
+                Edit
+              </Button>
+            </div>
           </Card>
         );
       })}
